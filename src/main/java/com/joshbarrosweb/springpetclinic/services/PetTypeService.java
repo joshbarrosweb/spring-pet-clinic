e5118b304baf28fca2196dd4737862c3bee39c70
Josue Barros <joshbarrosweb@gmail.com>
2022-04-02 20:35:27 -0300

    chore: added PetTypeService map interface and implementation

---
+++ src/main/java/com/joshbarrosweb/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.joshbarrosweb.springpetclinic.services;
+
+import com.joshbarrosweb.springpetclinic.entities.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
