5a7f6fec358ba55fa444cc0eae1292b5fffa40ba
Josue Barros <joshbarrosweb@gmail.com>
2022-04-02 21:02:32 -0300

    chore: added specialty service interface and implementation

---
+++ src/main/java/com/joshbarrosweb/springpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.joshbarrosweb.springpetclinic.services;
+
+import com.joshbarrosweb.springpetclinic.entities.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
