4542e16f7fdb0f710baf3b2d8d9cf449e8761617
Josue Barros <joshbarrosweb@gmail.com>
2022-04-02 23:03:15 -0300

    chore: added visit service interface and implementation

---
+++ src/main/java/com/joshbarrosweb/springpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.joshbarrosweb.springpetclinic.services;
+
+import com.joshbarrosweb.springpetclinic.entities.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
