9237ebbbef7030377d9c6bdf41de3f7f5bfab92d
Josue Barros <joshbarrosweb@gmail.com>
2022-04-02 21:59:34 -0300

    chore: JPA Annotations on Visit entity

--- src/main/java/com/joshbarrosweb/springpetclinic/entities/Visit.java
+++ src/main/java/com/joshbarrosweb/springpetclinic/entities/Visit.java
@@ -1,11 +1,20 @@
 package com.joshbarrosweb.springpetclinic.entities;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends Base {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+    
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
