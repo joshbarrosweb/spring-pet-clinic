8ce0ebcbfc1f925885f1f4869bc8cdaf7b80425f
Josue Barros <joshbarrosweb@gmail.com>
2022-04-02 21:54:50 -0300

    chore: JPA Annotations on Vet entity

--- src/main/java/com/joshbarrosweb/springpetclinic/entities/Specialty.java
+++ src/main/java/com/joshbarrosweb/springpetclinic/entities/Specialty.java
@@ -1,7 +1,14 @@
 package com.joshbarrosweb.springpetclinic.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends Base {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
