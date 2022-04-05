e132680bece38f55a49eddcab9bf29594c5a0cef
Josue Barros <joshbarrosweb@gmail.com>
2022-04-04 21:35:45 -0300

    chore: refactored into project lombok removing boilerplate code

--- src/main/java/com/joshbarrosweb/springpetclinic/entities/PetType.java
+++ src/main/java/com/joshbarrosweb/springpetclinic/entities/PetType.java
@@ -1,21 +1,20 @@
 package com.joshbarrosweb.springpetclinic.entities;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends Base {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
